6c67ad0152e3c0e6c522125213840df4d78f5eda
LukasGrunt <lukas.grunt25@gmail.com>
2025-01-18 19:47:53 +0200

    filled rating,transaction files, made columns and relationship

--- app/models/rating.py
+++ app/models/rating.py
@@ -1,1 +1,20 @@
 from app.database import db
+from father import Father
+
+class Rating(Father):
+    __tablename__ = 'ratings'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_id = db.Column(db.Integer, db.foreign_key("products.id"))
+    user_id = db.Column(db.Integer, db.foreign_key("users.id"))
+    rating = db.Column(db.Float)
+
+    product = db.relationship("Product", back_populates = "ratings")
+    user = db.relationship("User", back_populates = "ratings")
+
+    def __init__(self, rating):
+        self.rating = rating
+
+    def __repr__(self):
+        pass
+
+                           
