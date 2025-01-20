082d2863aa35bb863e80b6014d14c7989267f9b7
IgnasJ <email@example.com>
2025-01-20 17:28:36 +0200

    created registering and logging in functionality and tested

--- app/routes/users/user_routes.py
+++ app/routes/users/user_routes.py
@@ -1,51 +1,81 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app.database import db
 from app.models.user import User
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
-bp = Blueprint('users', __name__)
+user_routes = Blueprint('users', __name__)
 
-@bp.route('/register', methods = ['GET','POST'])
+@user_routes.route('/register', methods = ['GET','POST'])
 def register():
-    if request.method == 'GET':
-        user_email = request.form.get('user_email')
+    if request.method == 'POST':
+        login_email = request.form.get('user_email')
         password = request.form.get('password')
         name = request.form.get('name')
-        surname = request.form.get('surname')
-        is_admin = request.form.get('is_admin', 'false').lower() == 'true'
+        last_name = request.form.get('surname')
 
-        if not user_email or not password or not name or not surname:
+        if not login_email or not password or not name or not last_name:
             return render_template('create_user.html', error="All required fields must be filled in!")
     
-        if User.query.filter((User.login_email == user_email) | (User.password == password)).first():
+        if User.query.filter(User.login_email == login_email).first():
              return render_template('create_user.html', error="The email you entered is already registered, please try another one.")
     
-        password_hash = generate_password_hash(password_hash)
-        new_user = User(user_email=user_email, password=password_hash, name=name, surname=surname, is_admin=is_admin)
+        password_hash = generate_password_hash(password)
+        new_user = User(name=name, last_name = last_name, login_email= login_email, password=password_hash)
         db.session.add(new_user)
         db.session.commit()
 
-        return redirect(url_for('login'))
+        flash("Registration successful! Please log in.")
+        return redirect(url_for('users.login'))
+    
 
     return render_template('create_user.html')
 
-@bp.route('/login', methods=['GET', 'POST'])
+@user_routes.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get('email')
+        login_email = request.form.get('email')
         password = request.form.get('password')
 
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(login_email=login_email).first()
         if user and check_password_hash(user.password, password):
-            session['user_id'] = user.id
-            session['is_admin'] = user.is_admin
-            return redirect(url_for('dashboard'))
+            if not user.is_active or user.is_deleted:
+                return render_template('login.html', error="Your account is curently blocked or deleted.")
+            
+            login_user(user)  # user login using flask-login built in function
+            flash("Login successful.")
+            return redirect(url_for('users.dashboard'))
         else:
             return render_template('login.html', error="Invalid email or password entered.")
 
-    return render_template('login.html') 
+    return render_template('login.html')
 
-@bp.route('/logout', methods=['POST'])
+@user_routes.route('/logout')
+@login_required
 def logout():
-    session.clear()
-    return redirect(url_for('login'))
+    logout_user()
+    flash("You have successfully logged out!")
+    return redirect(url_for('users.login'))
+
+@user_routes.route('/dashboard')
+@login_required
+def dashboard():
+    if current_user.is_admin:
+        return redirect(url_for('users.admin_dashboard'))
+    else:
+        return redirect(url_for('users.user_dashboard'))
+    
+@user_routes.route('/admin_dashboard')
+@login_required
+def admin_dashboard():
+    if not current_user.is_admin:
+        return redirect(url_for('users.dashboard'))
+    return "Admin Dashboard!" #This can be changed to something more likeable
+
+
+@user_routes.route('/user_dashboard')
+@login_required
+def user_dashboard():
+    if current_user.is_admin:
+        return redirect(url_for('users.admin_dashboard'))
+    return "User Dashboard!" #This can be changed to something more likeable
