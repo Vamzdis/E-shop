a1af3b1443d1b933269d25f98e89a582b84e420e
Aigitstudy <dmitrijster@gmail.com>
2025-01-21 09:23:41 +0200

    added blueprint to routes

--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -1,11 +1,17 @@
 
-from app import app
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, Blueprint
 from app.models.product import Product
-from database import db
+from app.database import db
+from sqlalchemy import func
+from app.models.user import User
 
+admin = Blueprint("admin", __name__, url_prefix = "/admin")
 
-@app.route("/add_product", methods = ["GET", "POST"])
+@admin.route('/')
+def index():
+    return render_template('admin/index.html')
+
+@admin.route("/add_product", methods = ["GET", "POST"])
 def add_product():
     if request.method == "POST":
         try:
@@ -15,24 +21,36 @@ def add_product():
             quantity = int(request.form["quantity"])
         except ValueError as e: 
             error_message = str(e)
-            return render_template("...", error_message = error_message)
+            return render_template("...", error_message = error_message)        #add template
         
         is_available = quantity > 0
         is_deleted = False
+        created_on = func.now()
+        picture = "???"                             #add picture????
 
-        product = Product(name=name, description=description, price=price, quantity=quantity, is_available=is_available, is_deleted=is_deleted)
+        product = Product(
+            name=name, description=description, 
+            price=price, 
+            quantity=quantity, 
+            is_available=is_available, 
+            is_deleted=is_deleted,
+            created_on = created_on
+            )
+        
         db.session.add(product)
         db.session.commit()
-        return redirect(url_for("..."))         #add url
+        return redirect(url_for("..."))         #add template
     else:
-        return render_template("...")           #add url
+        return render_template("...")           #add template
 
-@app.route("/list_products")
+
+@admin.route("/list_products")
 def list_products():
     products = Product.query.all()
-    return render_template("...", products=products)    #add url
+    return render_template("admin/product_list.html", products=products)    #add template
 
-@app.route("/delete_producct/<int:id>", methods = ["GET", "POST"])
+
+@admin.route("/delete_producct/<int:id>", methods = ["GET", "POST"])
 def delete_product(id):
     product = Product.query.get(id)
 
@@ -42,11 +60,12 @@ def delete_product(id):
     if request.method == "POST":
         product.is_deleted = True
         db.session.commit()
-        return redirect(url_for("..."))         #add url
+        return redirect(url_for("..."))         #add template
     else:
-        return render_template("...", product=product)      #add url
+        return render_template("...", product=product)      #add template
 
-@app.route("/edit_product/<int:id>", methods = ["GET", "POST"])
+
+@admin.route("/edit_product/<int:id>", methods = ["GET", "POST"])
 def edit_product(id):
     product = Product.query.get(id)
 
@@ -62,13 +81,10 @@ def edit_product(id):
 
             if name:
                 product.name = name
-            
             if description:
                 product.description = description
-            
             if price:
                 product.price = float(price)
-
             if quantity:
                 product.quantity = int(quantity)
 
@@ -84,4 +100,34 @@ def edit_product(id):
             return render_template("...", error_message = error_message)    #add url
         return redirect(url_for("..."))
     return render_template("...", product = product)            #add url
+
+
+@admin.route("/list_users")
+def list_users():
+    users = Product.query.all()
+    return render_template("...", users=users)      #add template
+
+
+@admin.route("/delete_user/<int:id>", methods = ["GET", "POST"])
+def delete_user(id):
+    user = User.query.get(id)
+
+    if not user:
+        return "User not found", 404
+    
+    if request.method == "POST":
+        user.is_deleted = True
+        db.session.commit()
+        return redirect(url_for("..."))             #add template
+    else:
+        return render_template("...", user = user)  #add template
+    
+
+# Administratoriaus galimybės
+
+
+# Papildyti esamų prekių kiekį
+
+# Peržiūrėti statistika apie prekes. Kiek prekių nupirkta kurią dieną, už kiek nupirkta,
+# kurie mėnesiai pelningiausi, kurios prekės geriausiai įvertintos
 
