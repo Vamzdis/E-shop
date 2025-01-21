82ff6ac23e308dc2350f016e79937216a956737c
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-01-21 11:28:19 +0000

    created custom validators for logging in and registering (checks if user is registered, if the email is blocked/deleted, if password has all the requirements)

--- app/routes/users/user_routes.py
+++ app/routes/users/user_routes.py
@@ -1,17 +1,46 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for, flash
+from sqlalchemy import or_, and_
 from app.database import db
 from app.models.user import User
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp, ValidationError
+
+#custom validator at registration to check if the email has already been registered
+class EmailRegistered(object):
+    
+    def __init__(self, message=None):
+
+        if not message:
+            message = "The email you've entered is already registered."
+        self.message = message
+
+    def __call__(self, form, field):
+        email = field.data
+        if User.query.filter(User.login_email == email).first():
+            if User.query.filter(
+                User.login_email == email,
+                or_(
+                    User.is_active == False,  
+                    User.is_deleted == True)).first():
+                #if the user with this email was blocked or deleted already it would show this message
+                raise ValidationError(message="This email is already associated with a blocked or deleted account.")
+            
+            raise ValidationError(self.message)
 
 class RegistrationForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=3)])
     surname = StringField('Surname', validators=[DataRequired(), Length(min=3)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    email = StringField('Email', validators=[DataRequired(), Email(), EmailRegistered()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8), Regexp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_\-+=\[\]{}|\\:;"\'<>,.?/~`])',
+                                                    message="""The password must contain at least one 
+                                                    lower case letter, 
+                                                    upper case letter,
+                                                    number and a 
+                                                    special character
+                                                    """)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
@@ -33,9 +62,9 @@ def register():
         login_email = form.email.data
         password = form.password.data
 
-        if User.query.filter(User.login_email == login_email).first():
-            flash("The email you've entered ir registered already")
-            return render_template('user_register_extends_base.html', form=form)
+        # if User.query.filter(User.login_email == login_email).first():
+        #     flash("The email you've entered ir registered already")
+        #     return render_template('user_register_extends_base.html', form=form)
         
         password_hash = generate_password_hash(password) 
         new_user = User(name, surname, login_email, password_hash)
@@ -57,12 +86,18 @@ def login():
         password = form.password.data
 
         user = User.query.filter_by(login_email=login_email).first()
+        
         if user and check_password_hash(user.password, password):
-            if not user.is_active or user.is_deleted:
-                flash("Your account is blocked or deleted")
-                return render_template('user_login_extends_base.html', form = form)
+            if user.is_deleted or not user.is_active:
+                form.email.errors.append("This account is blocked or deleted.")
+                return render_template('login.html', form=form)
+            
+            # if not user.is_active or user.is_deleted:
+            #     flash("Your account is blocked or deleted")
+            #     return render_template('user_login_extends_base.html', form = form)
             
             login_user(user)  # user login using flask-login built in function
+
             flash("Login successful.")
             return redirect(url_for('users.dashboard'))
         else:
