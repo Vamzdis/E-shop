ee61a5517770f06d26dd4a1d4ff46bb4e8db16c4
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-01-21 07:40:03 +0000

    fixed user routes tabulation issue post merge

--- app/routes/users/user_routes.py
+++ app/routes/users/user_routes.py
@@ -32,22 +32,20 @@ def register():
         surname = form.surname.data
         login_email = form.email.data
         password = form.password.data
+    
+        if User.query.filter(User.login_email == login_email).first():
+            return render_template('create_user.html', error="The email you entered is already registered, please try another one.")
+        
+        password_hash = generate_password_hash(password) 
+        new_user = User(name, surname, login_email, password_hash)
+        db.session.add(new_user)
+        db.session.commit()
 
-    if not login_email or not password or not name or not surname:
-        return render_template('create_user.html', error="All required fields must be filled in!")
-    
-    if User.query.filter(User.login_email == login_email).first():
-        return render_template('create_user.html', error="The email you entered is already registered, please try another one.")
-    
-    password_hash = generate_password_hash(password) 
-    new_user = User(name, surname, login_email, password_hash)
-    db.session.add(new_user)
-    db.session.commit()
+        flash("Registration successful! Please log in.")
+        return redirect(url_for('users.login'))
 
-    flash("Registration successful! Please log in.")
-    return redirect(url_for('users.login'))
-    
-    # return render_template('user_register_extends_base.html', form = form)
+    return render_template('user_register_extends_base.html', form=form)
+ 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -57,11 +55,6 @@ def login():
     if form.validate_on_submit():
         login_email = form.email.data
         password = form.password.data
-
-
-    # if request.method == 'POST':
-    #     login_email = request.form.get('email')
-    #     password = request.form.get('password')
 
         user = User.query.filter_by(login_email=login_email).first()
         if user and check_password_hash(user.password, password):
@@ -73,17 +66,15 @@ def login():
             return redirect(url_for('users.dashboard'))
         else:
             return render_template('user_login_extends_base.html', form = form ,error="Invalid email or password entered.")
-
+        
+    return render_template('user_login_extends_base.html', form = form)
 
 @bp.route('/logout')
-    # return render_template('user_login_extends_base.html', form = form)
-
-# @user_routes.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     flash("You have successfully logged out!")
-#     return redirect(url_for('users.login'))
+@login_required
+def logout():
+    logout_user()
+    flash("You have successfully logged out!")
+    return redirect(url_for('users.login'))
 
 
 @bp.route('/dashboard')
