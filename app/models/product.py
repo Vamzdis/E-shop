813d5d8932ecaea870f15c1886146f1f47a7d852
IgnasJ <email@example.com>
2025-01-22 12:11:21 +0200

    added rating to init for product model

--- app/models/product.py
+++ app/models/product.py
@@ -20,12 +20,13 @@ class Product(db.Model):
     ratings = db.relationship("Rating",back_populates="product")
 
 
-    def __init__(self, name, description, price, picture, quantity, is_available, is_deleted, created_on=None):
+    def __init__(self, name, description, price, picture, quantity, rating, is_available, is_deleted, created_on=None):
         self.name = name
         self.description = description
         self.price = price
         self.picture = picture
         self.quantity = quantity
+        self.rating = rating
         self.created_on = created_on
         self.is_available = is_available
         self.is_deleted = is_deleted
