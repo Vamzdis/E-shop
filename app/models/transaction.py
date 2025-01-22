5ff3bbce62e1f85254577c0727caccee62d50c55
IgnasJ <email@example.com>
2025-01-22 10:32:50 +0200

    added status parameter to Transaction model

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -7,11 +7,14 @@ class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     sum = db.Column(db.Float)
+    status = db.Column(db.String(255), default = "Pending")
     created_on = db.Column(db.DateTime, default = func.now())
 
     user = db.relationship("User", back_populates="transactions")
 
-    def __init__(self, sum, created_on:datetime=None):
+    def __init__(self, user_id, sum, status, created_on:datetime=None):
+        self.user_id = user_id
+        self.status = status
         self.sum = sum
         self.created_on = created_on
 
