f38a0be44f24c0b5b8308774cd55d6e33115f41d
LinaVysnia <38082126+LinaVysnia@users.noreply.github.com>
2025-01-23 15:12:35 +0000

    some confusion happened with the homepage route. Could be moved in the future, but fixed some html names

--- app/routes/product_routes.py
+++ app/routes/product_routes.py
@@ -4,9 +4,7 @@ from flask_login import login_required, current_user
 
 bp = Blueprint("product", __name__)
 
-
 @bp.route('/products')
-@login_required
 def products():
     products = Product.query.all()
-    return render_template('products.html', products=products, user_logged_in=current_user.is_authenticated)
+    return render_template('products_extends_base.html', products=products)
